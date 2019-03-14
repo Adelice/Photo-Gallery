50b42599fcd5de603575ec6e577b1df569856f20
Adelice <nancytoniw@gmail.com>
2019-03-14 16:49:37 +0200

    create model location

--- photos/models.py
+++ photos/models.py
@@ -2,8 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    Image=models.ImageField(upload_to = 'images/')
+    image=models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length =30)
     description = models.CharField(max_length =30)
+    pub_date = models.DateTimeField(auto_now_add=True)
     # image_location = models.ForeignKey(Location)
     # image_category = models.ForeignKey(Category)
+    def __str__(self):
+        return self.name
+    def save_image(self):
+        self.save()    
+    class Meta:
+        ordering = ['name']    
+class Location(models.Model):
+    country = models.CharField(max_length =30)
+    city = models.CharField(max_length =30)
+    def __str__(self):
+        return self.country
+class Category(models.model):          
